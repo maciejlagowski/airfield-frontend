8a609f9656e1d556d07496f6fb83655eb2ee86ad
Maciej Lagowski <m_lagowski@o2.pl>
2020-10-23 15:59:46 +0200

    Bootstrap skeleton and "calendar"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeadbarComponent } from './components/headbar/headbar.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeadbarComponent,
+    CalendarComponent,
+    ModalDialogComponent
   ],
   imports: [
     BrowserModule
